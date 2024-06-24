24a5ea69763abd9d259a6ce90da01c6167fa4e5d
Maham Tahir <106860878+MahamTahir31@users.noreply.github.com>
2024-06-24 18:47:31 +0000

    updated

---
+++ vars.js
@@ -0,0 +1,8 @@
+const name = "Maham";
+let id = 1;
+var contact = "03123323443";
+gender = "female";
+
+// prefered not to use var because of issue in block scope and functional scope 
+
+console.table([name, id, contact, gender]);
